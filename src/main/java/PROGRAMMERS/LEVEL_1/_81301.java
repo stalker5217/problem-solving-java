bd6ade7f91f87b78ebd1ba24ae3cc623fc5d61d9
songmk <stalker5217@gmail.com>
2022-06-07 23:50:03 +0900

    숫자 문자열과 영단어

---
+++ src/main/java/PROGRAMMERS/LEVEL_1/_81301.java
@@ -0,0 +1,26 @@
+package PROGRAMMERS.LEVEL_1;
+
+public class _81301 {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution("one4seveneight"));
+	}
+
+	static class Solution {
+		public int solution(String s) {
+			s = s.replace("zero", "0");
+			s = s.replace("one", "1");
+			s = s.replace("two", "2");
+			s = s.replace("three", "3");
+			s = s.replace("four", "4");
+			s = s.replace("five", "5");
+			s = s.replace("six", "6");
+			s = s.replace("seven", "7");
+			s = s.replace("eight", "8");
+			s = s.replace("nine", "9");
+
+			return Integer.parseInt(s);
+		}
+	}
+}
+
