ac5b084e9010750e7809b4192cc6af9baf5a86c1
songmk <stalker5217@gmail.com>
2022-12-28 19:42:51 +0900

    100 : Same Tree

---
+++ src/main/java/LEETCODE/_100.java
@@ -0,0 +1,39 @@
+package LEETCODE;
+
+public class _100 {
+	// Definition for a binary tree node.
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	private static class Solution {
+		public boolean isSameTree(TreeNode p, TreeNode q) {
+			if (p == null && q == null) {
+				return true;
+			} else if (p == null && q != null) {
+				return false;
+			} else if (p != null && q == null) {
+				return false;
+			} else if (p.val != q.val) {
+				return false;
+			}
+
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+	}
+}
