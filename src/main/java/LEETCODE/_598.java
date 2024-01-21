1f2c5d68dd269b367e57d233bbc752ed9408e8cb
songmk <stalker5217@gmail.com>
2024-01-22 01:09:52 +0900

    598 : Range Addition II

---
+++ src/main/java/LEETCODE/_598.java
@@ -0,0 +1,17 @@
+package LEETCODE;
+
+public class _598 {
+    private static class Solution {
+        public int maxCount(int m, int n, int[][] ops) {
+            int minRow = m;
+            int minCol = n;
+
+            for (int[] op : ops) {
+                minRow = Math.min(minRow, op[0]);
+                minCol = Math.min(minCol, op[1]);
+            }
+
+            return minRow * minCol;
+        }
+    }
+}
